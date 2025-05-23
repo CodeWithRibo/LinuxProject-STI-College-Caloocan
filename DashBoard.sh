94d22f8e414e8fec0afba6422694002b5c8a484e
CodeWithRibo <riboluna2818@gmail.com>
2025-05-23 22:26:27 +0800

    added set password

--- DashBoard.sh
+++ DashBoard.sh
@@ -12,65 +12,55 @@ while [ $option -ne 6 ]; do
     echo "[3] List Folder and File"
     echo "[4] Delete a folder or file"
     echo "[5] Take notes"
-    echo "[6] Exit"
+    echo "[6] Set Password the File or Folder"
+    echo "[7] Exit"
 
     read -p "Select an option: " option
 
     case $option in
         1)
             read -p "Enter folder name: " folderName
-            if [ -d "$folderName" ]; then
-                echo "Folder already exists"
-            else
-                mkdir "$folderName"
-                echo "Folder $folderName has been created."
-                # read -p "Do you want to set a permission for that folder? (Y/N): " permission
-                # if [ "$permission" = "Y" ] || [ "$permission" = "y" ]; then
-                #     read -p "Enter permission (ex: 700): " setPermission
-                #     chmod "$setPermission" "$folderName"
-                #     echo "Permission $setPermission has been set for folder $folderName"
-                # fi
-            fi
+            [ ! -d $folderName ] && echo "$(mkdir $folderName)Folder $folderName has been created " || echo "Folder already exists"  
             ;;
         2)
             read -p "Enter file name: " fileName
-            if [ -f "$fileName" ]; then
-                echo "File already exists"
-            else
-                touch "$fileName"
-                echo "File $fileName has been created."
-                # read -p "Do you want to set a permission for that file? (Y/N): " permission
-                # if [ "$permission" = "Y" ] || [ "$permission" = "y" ]; then
-                #     read -p "Enter permission (ex: 700): " setPermission
-                #     chmod "$setPermission" "$fileName"
-                #     echo "Permission $setPermission has been set for file $fileName"
-                # fi
-            fi
+            [ ! -f $fileName ] && echo "$(touch $fileName)File $fileName has been created. " || echo "File already exists" 
             ;;
         3)
             list #Function
+
+            inputSelection=0 
+
+            until [ $inputSelection -eq 4 ]; do            
             echo "[1] Rename Files or Directories"
-            echo "[2] Set Permission Files or Directories"
+            echo "[2] Move files or Directories"
+            echo "[3] Set Permission Files or Directories"
+            echo "[4] Back to Dashboard"
             read -p "Choose selection : " inputSelection
 
+            #Nested Switch Case
             case $inputSelection in
             1)
-                read -p "Input File or Directory Name : " file_directory_name
+                read -p "Enter File or Directory Name : " file_directory_name
 
                 if [ -e $file_directory_name ]; then
-                read -p "Input New Name: " new_name
+                read -p "Enter New Name: " new_name
                 mv $file_directory_name $new_name
                 echo "You have sucessfully renamed $file_directory_name to $new_name"
                 else
                 echo "$file_directory_name does not exist"
                 fi
                 ;;
-            2) 
-                read -p "Enter the name of the file or directory for which you want to set permissions: " setUserPermission
-                if  [ ! -e $setUserPermission ]; then 
-                    echo "File does not exist"
-                    continue
+            2)
+                read -p "Enter the File or Directory you want to move: " moveFilesAndDirectories
+                if [ -e "$moveFilesAndDirectories" ]; then
+                    read -p "Enter the Destination path : " destination
+                    [ -e $destination ] && mv "$moveFilesAndDirectories" "$destination" || echo "Destination path does not exist"
                 fi
+                ;;
+            3) 
+                read -p "Enter the name of the File or Directory for which you want to set permissions: " setUserPermission
+                [ ! -e "$setUserPermission" ] && { echo "File or Directory does not exist"; continue; }
 
                 read -p "Enter permission (ex: 700): " setPermission                
                 if [ ! $setPermission -ge 778 ]; then 
@@ -81,9 +71,11 @@ while [ $option -ne 6 ]; do
                     echo "set permission denied"
                 fi
                 ;;
-            *) 
+            4) continue ;;
+            * ) 
                 echo "Invalid Input. Please try again.";;
             esac
+            done
             ;;
         4)
             read -p "Enter the content (file or folder) you want to delete: " deleteContent
@@ -105,15 +97,13 @@ while [ $option -ne 6 ]; do
         5)
             listFiles #Function
             read -p "Enter file you want to take a note: " inputUser
-
-            if [ -f $inputUser ]; then
-            takeNotes #Function
-            else
-            echo "$inputUser is not in the file selection "
-            fi
-
+            [ -f $inputUser ]  && takeNotes || echo "$inputUser is not in the file selection "
             ;;
-        6)  echo "Thank your for choosing GMP Linux File Explorer "
+        6)
+          list
+                     
+        ;;
+        7)  echo "Thank your for choosing GMP Linux File Explorer "
             ;;
 
         *)
